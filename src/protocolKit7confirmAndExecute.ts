892b90182b5e613a90baff358eae70f5bd3f1f46
HyukSang <zzooppii@gmail.com>
2025-09-09 16:42:28 +0900

    Create protocolKit7confirmAndExecute.ts

---
+++ src/protocolKit7confirmAndExecute.ts
@@ -0,0 +1,147 @@
+// This script demonstrates how to sign a transaction using the Safe Protocol Kit.
+// Please replace the placeholder values with your actual data.
+
+// You need to install the following packages:
+// npm install @safe-global/protocol-kit ethers dotenv
+
+import 'dotenv/config'
+import Safe, {
+    buildContractSignature,
+    buildSignatureBytes,
+} from '@safe-global/protocol-kit'
+import {
+    OperationType,
+    SafeSignature,
+    SigningMethod,
+  } from "@safe-global/types-kit"
+import SafeApiKit from '@safe-global/api-kit'
+import type { Hex } from "viem"
+import { createWalletClient, http } from "viem"
+import { privateKeyToAccount } from "viem/accounts"
+import { sepolia } from "viem/chains"
+
+// Load environment variables
+const RPC_URL = process.env.RPC_URL;
+const OWNER_PRIVATE_KEY = process.env.OWNER_PRIVATE_KEY;
+const OWNER_PRIVATE_KEY2 = process.env.OWNER_PRIVATE_KEY2;
+const SAFE_ADDRESS = process.env.SAFE_ADDRESS;
+const DAO_ADDRESS = process.env.DAO_ADDRESS;
+const SAFE_API_KEY = process.env.SAFE_API_KEY;
+
+async function main(): Promise<void> {
+    // 3. Safe 트랜잭션 데이터 생성
+    const safeTransactionData = {
+      "to": "0xf0B595d10a92A5a9BC3fFeA7e79f5d266b6035Ea",
+      "data": "0x",
+      "value": "1000000000000000",
+      "operation": 0,
+      "baseGas": "0",
+      "gasPrice": "0",
+      "gasToken": "0x0000000000000000000000000000000000000000",
+      "nonce": 3,
+      "refundReceiver": "0x0000000000000000000000000000000000000000",
+      "safeTxGas": "0"
+    }
+
+    const apiKit = new SafeApiKit({
+      chainId: 11155111n,
+      apiKey: SAFE_API_KEY
+    });
+
+    let protocolKit = await Safe.init({
+      provider: RPC_URL!,
+      safeAddress: SAFE_ADDRESS!,
+    })
+
+    let safeTx = await protocolKit.createTransaction({
+      transactions: [
+          safeTransactionData
+      ],
+    })
+
+    // console.log(SigningMethod)
+
+    let multiSigSigns = await protocolKit
+      .connect({
+        signer: OWNER_PRIVATE_KEY,
+        safeAddress: DAO_ADDRESS,
+      })
+      .then((k) =>
+        k.signTransaction(
+          safeTx,
+          SigningMethod.SAFE_SIGNATURE,
+          SAFE_ADDRESS
+        )
+      )
+    console.log("multiSigSigns1", multiSigSigns)
+
+    multiSigSigns = await protocolKit
+      .connect({
+        signer: OWNER_PRIVATE_KEY2,
+        safeAddress: DAO_ADDRESS,
+      })
+      .then((k) =>
+        k.signTransaction(
+          multiSigSigns,
+          SigningMethod.SAFE_SIGNATURE,
+          SAFE_ADDRESS
+        )
+      )
+      console.log("multiSigSigns2", multiSigSigns)
+    
+    const contractSignature = await buildContractSignature(
+      Array.from(multiSigSigns.signatures.values()),
+      DAO_ADDRESS!
+    )
+    safeTx.addSignature(contractSignature)
+    
+    const pendingTxs = await apiKit.getPendingTransactions(
+      SAFE_ADDRESS!
+    )
+    
+    const transaction = await apiKit.getTransaction(
+      pendingTxs.results[0].safeTxHash
+    )
+
+    const orginSign = await protocolKit
+      .toSafeTransactionType(transaction)
+      .then((safeTx) => Array.from(safeTx.signatures.values())[0])
+
+    const safeTxHash = await protocolKit.getTransactionHash(safeTx)
+    console.log("safeTxHash", safeTxHash)
+    const signatureResponse = await apiKit.confirmTransaction(
+      safeTxHash,
+      buildSignatureBytes([
+        orginSign,
+        safeTx.getSignature(DAO_ADDRESS!) as SafeSignature,
+      ])
+    )
+    console.log("signatureResponse", signatureResponse)
+
+    const safeTransaction = await protocolKit.toSafeTransactionType(transaction)
+    safeTransaction.encodedSignatures = () => {
+      return signatureResponse.signature
+    }
+    const data = await protocolKit.getEncodedTransaction(safeTransaction)
+
+    const account = privateKeyToAccount(process.env.SAFE_SIGNER1_PRIVATE_KEY as Hex)
+    const client = createWalletClient({
+      account,
+      chain: sepolia,
+      transport: http("https://eth-sepolia.api.onfinality.io/public"),
+    })
+
+    const hash = await client.sendTransaction({
+      to: SAFE_ADDRESS as `0x${string}`,
+      data: data as Hex,
+    })
+    console.log(hash)
+
+}
+
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
