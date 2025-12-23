803c4a15be64258cd9b6da4fed0167af3ca33e23
HyukSang <zzooppii@gmail.com>
2025-12-23 22:11:53 +0900

    Update confirmAndExecuteTestTRH.ts

--- src/3required/confirmAndExecuteTestTRH.ts
+++ src/3required/confirmAndExecuteTestTRH.ts
@@ -20,15 +20,15 @@ import { sepolia } from "viem/chains"
 
 
 const RPC_URL = process.env.RPC_URL;
-const SAFE_ADDRESS = "0x24d16804218C2279bf6A4B3cB88e9c929B903409";
+const SAFE_ADDRESS = "0x2FA87C9845411cC45Aa47fDA41a2e6b2421a61Bd";
 const DAO_ADDRESS = "0xA2101482b28E3D99ff6ced517bA41EFf4971a386";
 const SAFE_API_KEY = process.env.SAFE_API_KEY;
 
 
 async function main(): Promise<void> {
     let safeTransactionData = {
-        "to": "0x96091688580Bc75E51EeC46Db04560123CD73127",
-        "data": "0x7eff275e0000000000000000000000009e5caa79a304c41a0d91418b1005ef9074e989c30000000000000000000000007220c734653ae8ca014d4d82a84041ee4169499c",
+        "to": "0x1Db4C4a6cb1C86f6D266A9D4D520da5E0ADD2668",
+        "data": "0x7eff275e000000000000000000000000277a690e99c4197d07c29ed57090441dcb384b300000000000000000000000007220c734653ae8ca014d4d82a84041ee4169499c",
         "value": "0",
         "operation": 0,
         "baseGas": "0",
@@ -39,22 +39,24 @@ async function main(): Promise<void> {
         "safeTxGas": "0"
     }
 
+    let txHash = "0x1068a0ed4ccb47014493b58923bdf2794c92599df99fff7f5bac56285a4be2a2"
 
-    const rawData = {
-        "signer": "0x7220c734653ae8Ca014d4D82A84041EE4169499c",
-        "signature": "0x405fde30aa47fcb434d1fdfd076157bb99bb6e3011ff2020868d8f4f2f80255e2b0349a35b6a57ded5280427da8cdb24ddbe934c51d58971361ae25e31dfee281c"
-    };
 
-    const rawData2 = {
-        "signer": "0xeEfb04f3A4406363b399A1c3d194d75ca81A2d1B",
-        "signature": "0xc5356b2bc34be1d0e238bee40efba33b0b49a5272513c2a2d184d577c4a912dc51fe3e4d89b830a9f42e566d047f4cc1145c5fa4694646cc0183bd6716682cbf1b"
-    }
+    // const rawData = {
+    //     "signer": "0x7220c734653ae8Ca014d4D82A84041EE4169499c",
+    //     "signature": "0xd11c5c69676709634d486393745320fe59985522eadf55a3dce32ade77a93d910188bd3da2c92d01f172bdb73e014eafb4008cea604c44b424f372db5a5146331c"
+    // };
 
-    const ethSafeSignature = new EthSafeSignature(rawData.signer, rawData.signature);
-    console.log(ethSafeSignature);
+    // const rawData2 = {
+    //     "signer": "0xeEfb04f3A4406363b399A1c3d194d75ca81A2d1B",
+    //     "signature": "0x582b84d4684f87eacf827e08d8b2ca28465912d01e6a7ec18e896c021155ed5b37f90d585c2ad63f52a6f007f42a9505091863ec445c623fb248f9471e98cfcc1c"
+    // }
 
-    const ethSafeSignature2 = new EthSafeSignature(rawData2.signer, rawData2.signature);
-    console.log(ethSafeSignature2);
+    // const ethSafeSignature = new EthSafeSignature(rawData.signer, rawData.signature);
+    // console.log(ethSafeSignature);
+
+    // const ethSafeSignature2 = new EthSafeSignature(rawData2.signer, rawData2.signature);
+    // console.log(ethSafeSignature2);
 
     const apiKit = new SafeApiKit({
         chainId: 11155111n,
@@ -101,7 +103,8 @@ async function main(): Promise<void> {
     console.log("txHashData", txHashData)
 
     let safeTxHash = await protocolKit.getTransactionHash(safeTx)
-    console.log("safeTxHash : ", safeTxHash)
+    console.log("get safeTxHash : ", safeTxHash)
+    console.log("real safeTxHash : ", txHash)
 
     multiSigSigns = await protocolKit
         .connect({
@@ -122,30 +125,31 @@ async function main(): Promise<void> {
     )
     console.log("contractSignature :", contractSignature)
 
-    safeTx.addSignature(ethSafeSignature)
-    safeTx.addSignature(ethSafeSignature2)
+    // safeTx.addSignature(ethSafeSignature)
+    // safeTx.addSignature(ethSafeSignature2)
     safeTx.addSignature(contractSignature)
-    console.log("safeTx :", safeTx)
+    // console.log("safeTx :", safeTx)
 
     // console.log(safeTx.getSignature(DAO_ADDRESS!) as EthSafeSignature)
 
-    // const transaction = await apiKit.getTransaction(
-    //     safeTxHash
-    // )
-    // console.log("3")
+    // console.log("1")
+    const transaction = await apiKit.getTransaction(
+        safeTxHash
+    )
+    // console.log("2")
 
-    // const orginSign = await protocolKit
-    //     .toSafeTransactionType(transaction)
-    //     .then((safeTx) => Array.from(safeTx.signatures.values())[0])
+    const orginSign = await protocolKit
+        .toSafeTransactionType(transaction)
+        .then((safeTx) => Array.from(safeTx.signatures.values())[0])
 
-    // console.log("orginSign :", orginSign)
-    // // console.log("orginSign.length :", orginSign.data.length)
+    console.log("orginSign :", orginSign)
+    // console.log("orginSign.length :", orginSign.data.length)
 
-    // const orginSign2 = await protocolKit
-    //     .toSafeTransactionType(transaction)
-    //     .then((safeTx) => Array.from(safeTx.signatures.values())[1])
-    // console.log("orginSign2 :", orginSign2)
-    // // console.log("orginSign2.length :", orginSign2.data.length)
+    const orginSign2 = await protocolKit
+        .toSafeTransactionType(transaction)
+        .then((safeTx) => Array.from(safeTx.signatures.values())[1])
+    console.log("orginSign2 :", orginSign2)
+    // console.log("orginSign2.length :", orginSign2.data.length)
 
     // const orginSign3 = await protocolKit
     //     .toSafeTransactionType(transaction)
@@ -153,35 +157,34 @@ async function main(): Promise<void> {
     // console.log("orginSign3 :", orginSign3)
 
 
-    const signatureBytes = buildSignatureBytes([
-        ethSafeSignature,
-        ethSafeSignature2,
-        safeTx.getSignature(DAO_ADDRESS!) as SafeSignature,
-    ])
+    // const signatureBytes = buildSignatureBytes([
+    //     ethSafeSignature,
+    //     ethSafeSignature2,
+    //     safeTx.getSignature(DAO_ADDRESS!) as SafeSignature,
+    // ])
 
-    console.log("signatureBytes :", signatureBytes)
+    // console.log("signatureBytes :", signatureBytes)
 
-    // const signatureResponse = await apiKit.confirmTransaction(
-    //     safeTxHash,
-    //     buildSignatureBytes([
-    //         ethSafeSignature,
-    //         ethSafeSignature2,
-    //         safeTx.getSignature(DAO_ADDRESS!) as SafeSignature,
-    //     ])
-    // )
-    // console.log("signatureResponse :", signatureResponse)
+    const signatureResponse = await apiKit.confirmTransaction(
+        safeTxHash,
+        buildSignatureBytes([
+            orginSign,
+            orginSign2,
+            safeTx.getSignature(DAO_ADDRESS!) as SafeSignature,
+        ])
+    )
+    console.log("signatureResponse :", signatureResponse)
 
-    // const safeTransaction = await protocolKit.toSafeTransactionType(transaction)
-    const signature = '0x405fde30aa47fcb434d1fdfd076157bb99bb6e3011ff2020868d8f4f2f80255e2b0349a35b6a57ded5280427da8cdb24ddbe934c51d58971361ae25e31dfee281c000000000000000000000000a2101482b28e3d99ff6ced517ba41eff4971a38600000000000000000000000000000000000000000000000000000000000000c300c5356b2bc34be1d0e238bee40efba33b0b49a5272513c2a2d184d577c4a912dc51fe3e4d89b830a9f42e566d047f4cc1145c5fa4694646cc0183bd6716682cbf1b00000000000000000000000000000000000000000000000000000000000000825d72a57e565b5d05c9b0de12dab8ba5259f8636242016e587da7255bf24a2a6f0a5259eb6e86faee26e7442820d78ea6049d52df975931e2e953b10149165dea1f1b6d0977aa31da18e51f996fe2d68b6e790b3fb8719dd7649638b08e49c14d0251a77b138470e748ef201c5dd51df01714e3b9a9c672ffdba4d8c6eccd147a1a1f'
-    safeTx.encodedSignatures = () => {
-        return signature
+    const safeTransaction = await protocolKit.toSafeTransactionType(transaction)
+    safeTransaction.encodedSignatures = () => {
+        return signatureResponse.signature
     }
-    console.log("safeTx2 :", safeTx)
+    console.log("safeTransaction :", safeTransaction)
 
 
-    const data = await protocolKit.getEncodedTransaction(safeTx)
+    const data = await protocolKit.getEncodedTransaction(safeTransaction)
 
-    // console.log("data :", data)
+    console.log("data :", data)
 
     const account = privateKeyToAccount(process.env.SAFE_WALLET_OWNERKEY as Hex)
     const client = createWalletClient({
